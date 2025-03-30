cf1536691574161e409e342a15b7d5cce78c9536
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-03-29 19:17:20 -0600

    2nd commit. Created the theme for the application.

--- app/src/main/java/com/example/extendedtodo/MainActivity.kt
+++ app/src/main/java/com/example/extendedtodo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.extendedtodo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.extendedtodo.ui.theme.TaskListAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            TaskListAppTheme {
 
+            }
+        }
     }
 }
